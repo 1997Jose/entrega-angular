fd3004c16127f7365f3a03c5e9a6e9be68de6832
Jose <Jmhr1997@gmail.com>
2025-10-15 21:43:23 -0300

    ...

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './navbar/navbar';
+import { Toolbar } from './toolbar/toolbar';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Navbar,
+    Toolbar
   ],
   imports: [
     BrowserModule,
